bd4c6d4c6d0dc15c626661ce4bf9995d7e2fa79b
rob <rob@c269905c-fc1c-493a-b787-6b72c43b3874>
2007-05-22 05:19:51 +0000

    converted to filelocks
    git-svn-id: file:///home/rob/.svk/local/aros/lock/src@26597 c269905c-fc1c-493a-b787-6b72c43b3874

--- rom/dos/waitforchar.c
+++ rom/dos/waitforchar.c
@@ -65,8 +65,8 @@
     /* Prepare I/O request. */
     InitIOFS(&iofs, FSA_WAIT_CHAR, DOSBase);
 
-    iofs.IOFS.io_Device = fh->fh_Device;
-    iofs.IOFS.io_Unit   = fh->fh_Unit;
+    iofs.IOFS.io_Device = ((struct FileLock *) fh->fh_Arg1)->fl_Device;
+    iofs.IOFS.io_Unit   = ((struct FileLock *) fh->fh_Arg1)->fh_Unit;
 
     iofs.io_Union.io_WAIT_CHAR.io_Timeout = timeout;
 
