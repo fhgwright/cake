86d890bc8de3e8b52f98447b2e53a80376eb51d2
rob <rob@c269905c-fc1c-493a-b787-6b72c43b3874>
2007-05-24 21:23:15 +0000

    store device/unit in filehandle directly
    git-svn-id: file:///home/rob/.svk/local/aros/lock/src@26669 c269905c-fc1c-493a-b787-6b72c43b3874

--- rom/dos/write.c
+++ rom/dos/write.c
@@ -64,9 +64,8 @@
 
     /* Make sure the input parameters are sane. */
     ASSERT_VALID_PTR(fh);
-    ASSERT_VALID_PTR(fh->fh_Arg1);
-    ASSERT_VALID_PTR(((struct FileLock *) BADDR(fh->fh_Arg1))->fl_Unit);
-    ASSERT_VALID_PTR(((struct FileLock *) BADDR(fh->fh_Arg1))->fl_Device);
+    ASSERT_VALID_PTR(fh->fh_Device);
+    ASSERT_VALID_PTR(fh->fh_Unit);
     ASSERT_VALID_PTR(buffer);
 
     /* Handle append mode. */
@@ -78,8 +77,8 @@
     /* Prepare I/O request */
     InitIOFS( &iofs, FSA_WRITE, DOSBase );
 
-    iofs.IOFS.io_Device = ((struct FileLock *) BADDR(fh->fh_Arg1))->fl_Device;
-    iofs.IOFS.io_Unit   = ((struct FileLock *) BADDR(fh->fh_Arg1))->fl_Unit;
+    iofs.IOFS.io_Device = fh->fh_Device;
+    iofs.IOFS.io_Unit   = fh->fh_Unit;
 
     iofs.io_Union.io_WRITE.io_Buffer = (APTR)buffer;
     iofs.io_Union.io_WRITE.io_Length = length;
