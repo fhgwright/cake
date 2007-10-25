0810c07765509aaa1c1f9b054b02d915db65d71e
rob <rob@cataclysm.cx>
2007-10-25 20:59:17 +1000

    go with a trivial implementation; at least it now compiles

--- compiler/clib/__stack_chk_fail.c
+++ compiler/clib/__stack_chk_fail.c
@@ -2,12 +2,17 @@
 
 #if AROS_STACK_PROTECTOR
 
+/*
 #include <stdio.h>
 #include <stdlib.h>
+*/
 
 void __stack_chk_fail(void) {
+    asm("int $3");
+    /*
     fprintf(stderr, "stack smashing detected\n");
     exit(10);
+    */
 }
 
 #endif
--- compiler/clib/__stack_chk_fail_kernel.c
+++ compiler/clib/__stack_chk_fail_kernel.c
@@ -2,17 +2,22 @@
 
 #if AROS_STACK_PROTECTOR
 
+/*
 #include <exec/execbase.h>
 #include <exec/alerts.h>
 #include <proto/exec.h>
 #include <aros/arossupportbase.h>
+*/
 
 void __stack_chk_fail(void) {
+    asm("int $3");
+    /*
     struct AROSSupportBase *AROSSupportBase = (struct AROSSupportBase *) SysBase->DebugData;
 
     AROSSupportBase->kprintf("stack smashing detected\n");
 
     Alert(AG_BadParm);
+    */
 }
 
 #endif
