6adc7b3dd7bcbfd75629e43aa206b3fdfef17838
rob <rob@cataclysm.cx>
2007-11-27 07:12:39 +1100

    condition alloc/dealloc

--- workbench/libs/thread/createthreadcondition.c
+++ workbench/libs/thread/createthreadcondition.c
@@ -1,11 +1,19 @@
 #include "thread_intern.h"
 
+#include <exec/memory.h>
+#include <proto/exec.h>
+
 AROS_LH0(_ThreadCondition, CreateThreadCondition,
          struct ThreadBase *, ThreadBase, 14, Thread)
 {
     AROS_LIBFUNC_INIT
 
-    return NULL;
+    _ThreadCondition *cond;
+
+    if ((cond = AllocMem(sizeof(struct _ThreadCondition), MEMF_PUBLIC | MEMF_CLEAR)) == NULL)
+        return NULL;
+
+    return cond;
 
     AROS_LIBFUNC_EXIT
 }
--- workbench/libs/thread/destroythreadcondition.c
+++ workbench/libs/thread/destroythreadcondition.c
@@ -1,12 +1,17 @@
 #include "thread_intern.h"
 
+#include <proto/exec.h>
+#include <assert.h>
+
 AROS_LH1(void, DestroyThreadCondition,
-         AROS_LHA(ThreadCondition, condition, A0),
+         AROS_LHA(ThreadCondition, cond, A0),
          struct ThreadBase *, ThreadBase, 15, Thread)
 {
     AROS_LIBFUNC_INIT
 
+    assert(cond != NULL);
 
+    FreeMem(cond, sizeof(struct _ThreadCondition));
 
     AROS_LIBFUNC_EXIT
 }
