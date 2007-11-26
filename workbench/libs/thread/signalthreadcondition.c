8adf6252d870cafda839664c7e20b9f4fda3bf88
rob <rob@cataclysm.cx>
2007-11-27 08:52:29 +1100

    implemented SignalThreadCondition()

--- workbench/libs/thread/signalthreadcondition.c
+++ workbench/libs/thread/signalthreadcondition.c
@@ -1,12 +1,32 @@
 #include "thread_intern.h"
 
+#include <exec/tasks.h>
+#include <exec/lists.h>
+#include <proto/exec.h>
+#include <assert.h>
+
 AROS_LH1(void, SignalThreadCondition,
          AROS_LHA(_ThreadCondition, cond, A0),
          struct ThreadBase *, ThreadBase, 17, Thread)
 {
     AROS_LIBFUNC_INIT
 
+    _ThreadWaiter waiter;
 
+    assert(cond != NULL);
+
+    /* safely remove a waiter from the list */
+    ObtainSemaphore(&cond->lock);
+    waiter = (_ThreadWaiter) REMHEAD(&cond->waiters);
+    if (waiter != NULL)
+        cond->count--;
+    ReleaseSemaphore(&cond->lock);
+
+    /* signal the task */
+    Signal(waiter->task, SIGF_SINGLE);
+
+    /* all done */
+    FreeMem(waiter, sizeof(struct _ThreadWaiter));
 
     AROS_LIBFUNC_EXIT
 }
