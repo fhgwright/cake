6ab7bc58662ea2d1b61f23c578b595b9f53dbcb0
rob <rob@cataclysm.cx>
2007-11-27 10:12:48 +1100

    implemented BroadcastThreadCondition()

--- workbench/libs/thread/broadcastthreadcondition.c
+++ workbench/libs/thread/broadcastthreadcondition.c
@@ -1,12 +1,33 @@
 #include "thread_intern.h"
 
+#include <exec/tasks.h>
+#include <exec/lists.h>
+#include <proto/exec.h>
+#include <assert.h>
+
 AROS_LH1(void, BroadcastThreadCondition,
          AROS_LHA(_ThreadCondition, cond, A0),
          struct ThreadBase *, ThreadBase, 18, Thread)
 {
     AROS_LIBFUNC_INIT
 
+    _ThreadWaiter waiter;
 
+    assert(cond != NULL);
+
+    /* safely operation on the condition */
+    ObtainSemaphore(&cond->lock);
+
+    /* wake up all the waiters */
+    while ((waiter = (_ThreadWaiter) REMHEAD(&cond->waiters)) != NULL) {
+        Signal(waiter->task, SIGF_SINGLE);
+        FreeMem(waiter, sizeof(struct _ThreadWaiter));
+    }
+
+    /* none left */
+    cond->count = 0;
+
+    ReleaseSemaphore(&cond->lock);
 
     AROS_LIBFUNC_EXIT
 }
