43ea443fdb0a782916985ff89f464c4f39c7fc8b
rob <rob@cataclysm.cx>
2007-11-27 07:34:29 +1100

    don't destroy a condition if its in use

--- workbench/libs/thread/destroythreadcondition.c
+++ workbench/libs/thread/destroythreadcondition.c
@@ -5,14 +5,23 @@
 #include <assert.h>
 
 AROS_LH1(BOOL, DestroyThreadCondition,
-         AROS_LHA(ThreadCondition, cond, A0),
+         AROS_LHA(_ThreadCondition, cond, A0),
          struct ThreadBase *, ThreadBase, 15, Thread)
 {
     AROS_LIBFUNC_INIT
 
+    BOOL in_use = FALSE;
+
     assert(cond != NULL);
 
-    /* XXX make sure noone is waiting */
+    /* we can only destroy the cond if its not held and noone is waiting */
+    ObtainSemaphoreShared(&cond->lock);
+    if (cond->count > 0)
+        in_use = TRUE;
+    ReleaseSemaphore(&cond->lock);
+
+    if (in_use)
+        return FALSE;
 
     FreeMem(cond, sizeof(struct _ThreadCondition));
 
