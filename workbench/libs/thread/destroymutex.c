96b207b903147b1e064573142fd2dfb69153ee01
rob <rob@cataclysm.cx>
2007-11-27 07:25:38 +1100

    don't destroy mutexes/conditions if they're in use

--- workbench/libs/thread/destroymutex.c
+++ workbench/libs/thread/destroymutex.c
@@ -4,15 +4,28 @@
 #include <proto/exec.h>
 #include <assert.h>
 
-AROS_LH1(void, DestroyMutex,
+AROS_LH1(BOOL, DestroyMutex,
          AROS_LHA(_Mutex, mutex, A0),
          struct ThreadBase *, ThreadBase, 10, Thread)
 {
     AROS_LIBFUNC_INIT
 
+    BOOL in_use = FALSE;
+
     assert(mutex != NULL);
 
+    /* we can only destroy the mutex if its not held and noone is waiting */
+    Forbid();
+    if (((struct SignalSemaphore *) mutex)->ss_QueueCount >= 0)
+        in_use = TRUE;
+    Permit();
+
+    if (in_use)
+        return FALSE;
+
     FreeMem(mutex, sizeof(struct SignalSemaphore));
+
+    return TRUE;
 
     AROS_LIBFUNC_EXIT
 }
--- workbench/libs/thread/destroythreadcondition.c
+++ workbench/libs/thread/destroythreadcondition.c
@@ -1,9 +1,10 @@
 #include "thread_intern.h"
 
+#include <exec/types.h>
 #include <proto/exec.h>
 #include <assert.h>
 
-AROS_LH1(void, DestroyThreadCondition,
+AROS_LH1(BOOL, DestroyThreadCondition,
          AROS_LHA(ThreadCondition, cond, A0),
          struct ThreadBase *, ThreadBase, 15, Thread)
 {
@@ -11,7 +12,11 @@ AROS_LH1(void, DestroyThreadCondition,
 
     assert(cond != NULL);
 
+    /* XXX make sure noone is waiting */
+
     FreeMem(cond, sizeof(struct _ThreadCondition));
+
+    return TRUE;
 
     AROS_LIBFUNC_EXIT
 }
