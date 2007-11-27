47f857357aa42d45a8a28e589f39bd96280c1cdb
rob <rob@cataclysm.cx>
2007-11-28 09:42:22 +1100

    implemented DetachThread()

--- workbench/libs/thread/detachthread.c
+++ workbench/libs/thread/detachthread.c
@@ -1,12 +1,24 @@
 #include "thread_intern.h"
 
-AROS_LH1(void, DetachThread,
+AROS_LH1(BOOL, DetachThread,
          AROS_LHA(ThreadIdentifier, thread_id, D0),
          struct ThreadBase *, ThreadBase, 7, Thread)
 {
     AROS_LIBFUNC_INIT
 
+    _Thread thread = _getthreadbyid(thread_id, ThreadBase);
+    if (thread == NULL)
+        return FALSE;
 
+    ObtainSemaphore(&thread->lock);
+    if (thread->detached) {
+        ReleaseSemaphore(&thread->lock);
+        return FALSE;
+    }
+    thread->detached = TRUE;
+    ReleaseSemaphore(&thread->lock);
+
+    return TRUE;
 
     AROS_LIBFUNC_EXIT
 }
