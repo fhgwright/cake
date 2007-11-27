4742b480d68b83bad19a58d29224cdfc0fd6595c
rob <rob@cataclysm.cx>
2007-11-28 09:28:21 +1100

    implemented GetCurrentThread()

--- workbench/libs/thread/currentthread.c
+++ workbench/libs/thread/currentthread.c
@@ -1,11 +1,17 @@
 #include "thread_intern.h"
 
+#include <proto/exec.h>
+
 AROS_LH0(ThreadIdentifier, CurrentThread,
          struct ThreadBase *, ThreadBase, 8, Thread)
 {
     AROS_LIBFUNC_INIT
 
-    return 0;
+    _Thread thread = _getthreadbytask(FindTask(NULL), ThreadBase);
+    if (thread == NULL)
+        return -1;
+
+    return thread->id;
 
     AROS_LIBFUNC_EXIT
 }
