2ac3707157a11d416d5b895ac1f9ea8f1cf46c4a
rob <rob@cataclysm.cx>
2007-11-30 07:25:27 +1100

    kill threads on main task exit / library close

--- workbench/libs/thread/thread_init.c
+++ workbench/libs/thread/thread_init.c
@@ -17,10 +17,20 @@ static int GM_UNIQUENAME(Open)(struct ThreadBase *ThreadBase) {
 
 static int GM_UNIQUENAME(Close)(struct ThreadBase *ThreadBase) {
     int count;
+    _Thread thread;
 
     ListLength(&ThreadBase->threads, count);
-    if (count > 0)
-        kprintf("%d threads outstanding! brace yourself...\n", count);
+    if (count > 0) {
+        kprintf("[thread] %d threads still running, killing them\n", count);
+
+        ForeachNode(&ThreadBase->threads, thread) {
+            RemTask(thread->task);
+
+            kprintf("  %d (task 0x%p) killed\n", thread->id, thread->task);
+
+            FreeVec(thread);
+        }
+    }
 
     return TRUE;
 }
