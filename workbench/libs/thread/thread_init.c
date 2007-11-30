b85c7590b4e9d1e1742455586d1bad9207360b15
rob <rob@cataclysm.cx>
2007-11-30 22:39:43 +1100

    detach remaining threads when the main task exits

--- workbench/libs/thread/thread_init.c
+++ workbench/libs/thread/thread_init.c
@@ -12,6 +12,7 @@
 #include <exec/semaphores.h>
 #include <exec/lists.h>
 #include <proto/exec.h>
+#include <proto/thread.h>
 #include <aros/symbolsets.h>
 
 #include LC_LIBDEFS_FILE
@@ -25,56 +26,12 @@ static int GM_UNIQUENAME(Open)(struct ThreadBase *ThreadBase) {
 }
 
 static int GM_UNIQUENAME(Close)(struct ThreadBase *ThreadBase) {
-    int count;
-    _Thread thread;
+    _Thread thread, next;
 
-    /* XXX we're most likely here because main() exited. if there are
-     * remaining threads, we need to do something with them. we have the
-     * following options:
-     * 
-     * 1. leave them alone
-     * 2. wait for them to finish
-     * 3. kill them
-     *
-     * [1] is problematic because entry_trampoline() in createthread.c expects
-     * ThreadBase to be valid after the user thread function completes so it
-     * can do cleanup. ThreadBase will be invalid not long after this Close
-     * function completes, so entry_trampoline() will try to get the base lock
-     * and will fail and crash, usually taking AROS down with it.
-     * 
-     * [2] is fine, but there's no guarantee that they ever will finish;
-     * furthermore this Close function is inside Forbid() right now, which
-     * means we'd have to re-enable task switches. that's safe because this is
-     * a per-opener base, but its just a little bit tricky. also we need an
-     * extra condition variable and counter to watch all thread exits at once.
-     *
-     * [3] ensures that the threads are gone and the main task can exit right
-     * now, but AROS really doesn't provide a way to safely kill a process.
-     * RemTask() will make sure it never gets scheduled again and will free
-     * the memory it allocated, but it may have open libraries, filehandles,
-     * etc which will get leaked. This can't be fixed without proper task
-     * resource tracking.
-     *
-     * I've chosen [3] for now, because it seems to make the most sense in that
-     * once main exits, the process should no longer exists, but in reality
-     * they all suck. for now, the user should ensure that all the threads
-     * have completed before the main task exits.
-     */
-
-    ListLength(&ThreadBase->threads, count);
-    if (count > 0) {
-        kprintf("[thread] %d threads still running, killing them\n", count);
-
-        ForeachNode(&ThreadBase->threads, thread) {
-            RemTask(thread->task);
-
-            kprintf("  %d (task 0x%p) killed\n", thread->id, thread->task);
-
-            FreeVec(thread);
-        }
-
-        kprintf("[thread] system may be unstable! reboot as soon as possible\n");
-    }
+    /* detach any remaining threads. its hard to know what the right thing to
+     * do here is, but we have to do something */
+    ForeachNodeSafe(&ThreadBase->threads, thread, next)
+        DetachThread(thread->id);
 
     return TRUE;
 }
