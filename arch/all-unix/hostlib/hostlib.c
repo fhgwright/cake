45d525ea12d883c7e9bdec062e520586bb1ddebc
rob <rob@cataclysm.cx>
2009-05-22 16:31:07 +1000

    generic hostlib copied from mingw32 (though that should really just be made common)

--- arch/all-unix/hostlib/hostlib.c
+++ arch/all-unix/hostlib/hostlib.c
@@ -1,55 +1,38 @@
-#include <exec/types.h>
+#define DEBUG 0
+
+#include <aros/debug.h>
+#include <aros/kernel.h>
 #include <aros/libcall.h>
+#include <aros/symbolsets.h>
+#include <proto/exec.h>
+#include <proto/kernel.h>
+#include <proto/utility.h>
 
-#define timeval sys_timeval
-#include <dlfcn.h>
-#undef timeval
-
+#include "../kernel/hostinterface.h"
 #include "hostlib_intern.h"
 
 #include LC_LIBDEFS_FILE
 
-#define DEBUG 0
-#include <aros/debug.h>
-
 AROS_LH2(void *, HostLib_Open,
          AROS_LHA(const char *, filename, A0),
          AROS_LHA(char **,      error,    A1),
-         struct HostLibBase *, HostLibBase, 1, HostLib) {
-
+         struct HostLibBase *, HostLibBase, 1, HostLib)
+{
     AROS_LIBFUNC_INIT
 
-    void *handle;
-
-    D(bug("[hostlib] Open: filename=%s\n", filename));
-    
-    handle = dlopen((char *) filename, RTLD_NOW);
-
-    if (error != NULL)
-        *error = handle == NULL ? dlerror() : NULL;
-
-    return handle;
+    return HostLibBase->HostIFace->HostLib_Open(filename, error);
 
     AROS_LIBFUNC_EXIT
 }
 
-AROS_LH2(int, HostLib_Close,
+AROS_LH2(BOOL, HostLib_Close,
          AROS_LHA(void *,  handle, A0),
          AROS_LHA(char **, error,  A1),
-         struct HostLibBase *, HostLibBase, 2, HostLib) {
-
+         struct HostLibBase *, HostLibBase, 2, HostLib)
+{
     AROS_LIBFUNC_INIT
 
-    int ret;
-
-    D(bug("[hostlib] Close: handle=0x%08x\n", handle));
-
-    ret = dlclose(handle);
-
-    if (error != NULL)
-        *error = ret != 0 ? dlerror() : NULL;
-
-    return ret;
+    return HostLibBase->HostIFace->HostLib_Close(handle, error);
 
     AROS_LIBFUNC_EXIT
 }
@@ -58,32 +41,84 @@ AROS_LH3(void *, HostLib_GetPointer,
          AROS_LHA(void *,       handle, A0),
          AROS_LHA(const char *, symbol, A1),
          AROS_LHA(char **,      error,  A2),
-         struct HostLibBase *, HostLibBase, 3, HostLib) {
-
+         struct HostLibBase *, HostLibBase, 3, HostLib)
+{
     AROS_LIBFUNC_INIT
 
-    void *ptr;
-
-    D(bug("[hostlib] GetPointer: handle=0x%08x, symbol=%s\n", handle, symbol));
-
-    dlerror();
-
-    ptr = dlsym(handle, (char *) symbol);
-
-    if (error != NULL)
-        *error = dlerror();
-
-    return ptr;
+    return HostLibBase->HostIFace->HostLib_GetPointer(handle, symbol, error);
 
     AROS_LIBFUNC_EXIT
 }
 
-/* In libdl error strings are static, there's no need to free them */
 AROS_LH1(void, HostLib_FreeErrorStr,
     	 AROS_LHA(char *, error, A0),
     	 struct HostLibBase *, HostLibBase, 4, HostLib)
 {
     AROS_LIBFUNC_INIT
 
+    HostLibBase->HostIFace->HostLib_FreeErrorStr(error);
+
     AROS_LIBFUNC_EXIT
 }
+
+AROS_LH3(APTR *, HostLib_GetInterface,
+    	 AROS_LHA(void *,  handle,         A0),
+    	 AROS_LHA(const char **, symtable, A1),
+    	 AROS_LHA(ULONG *, unresolved,     A2),
+    	 struct HostLibBase *, HostLibBase, 5, HostLib)
+{
+    AROS_LIBFUNC_INIT
+
+    const char **c;
+    ULONG cnt = 0;
+    APTR *iface = NULL;
+
+    for (c = symtable; *c; c++)
+        cnt += sizeof(APTR);
+    if (cnt) {
+    	iface = AllocVec(cnt, MEMF_CLEAR);
+    	if (iface) {
+    	    cnt = HostLibBase->HostIFace->HostLib_GetInterface(handle, (char **) symtable, iface);
+    	    if (unresolved)
+    	        *unresolved = cnt;
+    	}
+    }
+    return iface;
+    
+    AROS_LIBFUNC_EXIT
+}
+
+AROS_LH1I(void, HostLib_DropInterface,
+   	 AROS_LHA(APTR *, interface, A0),
+   	 struct HostLibBase *, HostLibBase, 6, HostLib)
+{
+    AROS_LIBFUNC_INIT
+
+    FreeVec(interface);
+
+    AROS_LIBFUNC_EXIT
+}
+
+/* auto init */
+static int HostLib_Init(LIBBASETYPEPTR LIBBASE)
+{
+    struct UtilityBase *UtilityBase;
+    APTR KernelBase;
+    struct TagItem *BootInfo;
+    
+    UtilityBase = (struct UtilityBase *)OpenLibrary("utility.library", 36);
+    D(bug("[hostlib] UtilityBase = 0x%08lX\n", UtilityBase));
+    if (UtilityBase) {
+        KernelBase = OpenResource("kernel.resource");
+        D(bug("[hostlib] KernelBase = 0x%08lX\n", KernelBase));
+        if (KernelBase) {
+    	    BootInfo = KrnGetBootInfo();
+    	    HostLibBase->HostIFace = (struct HostInterface *)GetTagData(KRN_HostInterface, 0, BootInfo);
+    	    D(bug("[hostlib] HostIFace = 0x%08lX\n", HostLibBase->HostIFace));
+    	}
+    	CloseLibrary((struct Library *)UtilityBase);
+    }
+    return (int)HostLibBase->HostIFace;
+}
+
+ADD2INITLIB(HostLib_Init, 0)
