4224d87bccbb7a81eb172bc1cccd40fc41c64d88
rob <rob@cataclysm.cx>
2009-05-27 07:13:18 +1000

    use an enum for syscall ids

--- arch/all-unix/kernel/syscall.h
+++ arch/all-unix/kernel/syscall.h
@@ -1,14 +1,11 @@
 #ifndef SYSCALL_H_
 #define SYSCALL_H_
 
-#define SC_CAUSE        0
-#define SC_DISPATCH     1
-#define SC_SWITCH       2
-#define SC_SCHEDULE     3
-#define SC_CLI          4
-#define SC_STI          5
-#define SC_SUPERSTATE   6
-#define SC_ISSUPERSTATE 7
-#define SC_INVALIDATED  8
+typedef enum {
+    sc_CAUSE,
+    sc_DISPATCH,
+    sc_SWITCH,
+    sc_SCHEDULE
+} syscall_type_t;
 
 #endif /*SYSCALL_H_*/
